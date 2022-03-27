afef5ac9c07d1671a3c6609ce26f533904791c7b
nicojay <jayatilanonico28@gmail.com>
2022-03-27 18:33:20 +0800

    final changes

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -48,42 +48,26 @@ function HomeScreen() {
         <title>BANN Assistive Tech Store</title>
       </Helmet>
       <Carousel controls={false} fade>
-        <Carousel.Item>
+        <Carousel.Item interval={3000}>
           <img
             className="d-block w-100"
-            src="./images/c1.jpg"
+            src="./images/img1.jpg"
             alt="First slide"
           />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
+        <Carousel.Item interval={3000}>
           <img
             className="d-block w-100"
-            src="./images/c2.jpg"
+            src="./images/img2.jpg"
             alt="Second slide"
           />
-
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
+        <Carousel.Item interval={3000}>
           <img
             className="d-block w-100"
-            src="./images/c3.jpg"
+            src="./images/img3.jpg"
             alt="Third slide"
           />
-
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
       <h1>Featured Products</h1>
