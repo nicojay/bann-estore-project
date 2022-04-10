c9423b71615a0d90de175064cd69f92cdd5ed1b2
nicojay <jayatilanonico28@gmail.com>
2022-04-10 12:39:25 +0800

    last changes

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -131,7 +131,7 @@ function ProductScreen() {
         <Col md={6}>
           <img
             className="img-large"
-            src={product.image}
+            src={selectedImage || product.image}
             alt={product.name}
           ></img>
         </Col>
